3b57fb44738c2bfd452812380652bc895dd90eda
julianaruizp06 <107581690+julianaruizp06@users.noreply.github.com>
2024-08-29 15:15:21 -0500

    template

---
+++ clean-template/src/utils/constants.js
@@ -0,0 +1,7 @@
+export default {
+	language: 'es_mx',
+	siteData: {
+		siteUrl: 'https://slim-react-mui-templateEjruizp.netlify.app/',
+		siteRepo: 'https://github.com/julianaruizp06/slim-react-mui-template-free',
+	},
+};
---
+++ complete-template/src/utils/constants.js
@@ -0,0 +1,14 @@
+export default {
+	author: 'Antonio Ayola',
+	avatar: 'https://avatars.githubusercontent.com/u/35477201?v=4',
+	profilesUrls: {
+		linkedin: 'https://www.linkedin.com/in/antonio-ayola/',
+		github: 'https://github.com/julianaruizp06',
+		email: 'antonio.ayola.cortes@gmail.com',
+	},
+	language: 'es_mx',
+	siteData: {
+		siteUrl: 'https://slim-free-react-mui-template.netlify.app/',
+		siteRepo: 'https://github.com/julianaruizp06/slim-free-react-mui-template',
+	},
+};
